45b2ba8e79a21ae9917a731148db47358dfc6f82
Davi Wesley <davi.wesley@icloud.com>
2017-06-19 07:13:00 -0300

    Create Juiz.java

---
+++ src/Pessoas/Juiz.java
@@ -0,0 +1,15 @@
+package Pessoas;
+
+public class Juiz extends Funcionario{
+
+	public Juiz(String nome, String usuario, String titulo, Boolean privilegio, int senha) {
+		super(nome, usuario, titulo, privilegio, senha);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+
+
+
+}
