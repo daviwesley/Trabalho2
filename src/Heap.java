cf1aaeff6dfb3f1bc154873d94606c744df2fca2
Davi Wesley <davi.wesley@icloud.com>
2017-06-19 07:09:31 -0300

    Update Heap.java

--- src/Heap.java
+++ src/Heap.java
@@ -1,15 +1,17 @@
-package HeapMaximo;
+package HeapMax;
+
+import Processo.Processo;
 
 public class HeapMax {
 
-	private final int heap[] = new int[20];
+	private final Processo heap[] = new Processo[50];
 	private int tam;
 
 	public void subir(int i) {
 		int j = i / 2;
 		if (j >= 1) {
-			if (this.heap[i] > this.heap[j]) {
-				int aux = heap[i];
+			if (this.heap[i].getPrioridade() > this.heap[j].getPrioridade()) {
+				Processo aux = heap[i];
 				this.heap[i] = this.heap[j];
 				this.heap[j] = aux;
 			}
@@ -20,14 +22,14 @@ public class HeapMax {
 		int j = 2 * i;
 		if (j <= this.tam) {
 			if (j < this.tam) {
-				if (this.heap[j + 1] > this.heap[j]) {
+				if (this.heap[j + 1].getPrioridade() > this.heap[j].getPrioridade()) {
 					j = j + 1;
 				}
 			}
-			if (this.heap[i] < this.heap[j]) {
-				int aux = heap[i];
+			if (this.heap[i].getPrioridade() < this.heap[j].getPrioridade()) {
+				Processo aux = heap[i];
 				this.heap[i] = this.heap[j];
-				this.heap[j] = aux;
+				this.heap[j]= aux;
 				this.descer(j);
 			}
 		}
@@ -35,22 +37,22 @@ public class HeapMax {
 
 	public void add(int chave) {
 		if (this.tam < this.heap.length) {
-			this.heap[this.tam + 1] = chave;
+			this.heap[this.tam + 1].setPrioridade(chave);
 			this.tam++;
 			this.subir(tam + 1);
 		}
 	}
 
-	public int remover() {
+	public Processo remover() {
 		if (this.tam != 0) {
-			int aux = this.heap[1];
+			Processo aux = this.heap[1];
 			this.heap[1] = this.heap[this.tam];
 			this.tam--;
 			this.descer(1);
 			return aux;
 		} else {
 			System.out.println("VAZIO");
-			return -1;
+			return null;
 		}
 	}
 	
